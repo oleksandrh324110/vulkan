668e4dd020d084211ef706308bc6d3415f1f2938
Oleksandr Havrylutsa <oleksandr.havrylutsa@gmail.com>
2024-11-12 02:37:44 +0200

    added portability_enumeration as required extensino

--- src/engine/vulkan/create_instance.h
+++ src/engine/vulkan/create_instance.h
@@ -2,6 +2,7 @@
 
 #include <SDL_vulkan.h>
 #include <vulkan/vulkan.h>
+#include <vulkan/vulkan_core.h>
 
 extern VkInstance instance;
 extern SDL_Window *window;
@@ -16,8 +17,11 @@ void create_instance(SDL_Window *window) {
 
   uint32_t sdl_extension_count;
   SDL_Vulkan_GetInstanceExtensions(NULL, &sdl_extension_count, NULL);
-  const char **sdl_extensions = malloc(sdl_extension_count * (sizeof(char *)));
+  const char **sdl_extensions =
+      malloc(++sdl_extension_count * (sizeof(char *)));
   SDL_Vulkan_GetInstanceExtensions(NULL, &sdl_extension_count, sdl_extensions);
+  sdl_extensions[sdl_extension_count++] =
+      VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME;
 
   for (size_t i = 0; i < sdl_extension_count; i++)
     printf("%s\n", sdl_extensions[i]);
@@ -28,6 +32,7 @@ void create_instance(SDL_Window *window) {
       .enabledExtensionCount = sdl_extension_count,
       .ppEnabledExtensionNames = sdl_extensions,
       .enabledLayerCount = 0};
+  create_info.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
 
   if (vkCreateInstance(&create_info, NULL, &instance) != VK_SUCCESS) {
     fprintf(stderr, "failed to create instance!\n");
